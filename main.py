b94fa3b9646c4f17f1801562bdc29c1b53c45281
Steve Gossip <stephengossip@gmail.com>
2022-02-09 21:37:29 +0000

    sorted the list by highest occuring letter.

--- main.py
+++ main.py
@@ -3,20 +3,23 @@ file = open("wordle_words.txt", "r")
 wordle_list = []
 
 alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","x","y","z"]
-result = {}
+letter_totals = []
 for word in file:
     wordle_list.append(word.replace("\n",""))
 
 test_list = wordle_list[:10]
 print(test_list)
 for letter in alphabet:
-    result[letter] = 0
+    result = {"letter": letter,"total": 0}
     for word in wordle_list:
-        for l in word:
-            if letter == l:
-                result[letter] += 1
+        for char in word:
+            if letter == char:
+                result["total"] += 1
+    letter_totals.append(result)
 
-print(result)
+sorted_list = sorted(letter_totals, key=lambda d: d['total'], reverse=True)
+print(sorted_list)
+
 
 
 
