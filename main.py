58845ecdbec7cee852028697561cef29179c403e
Steve Gossip <stephengossip@gmail.com>
2022-02-09 18:11:39 +0000

    found the total number of uses of each letter throughout the wordle_list.

--- main.py
+++ main.py
@@ -2,12 +2,21 @@
 file = open("wordle_words.txt", "r")
 wordle_list = []
 
+alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","x","y","z"]
+result = {}
 for word in file:
-    wordle_list.append(word)
+    wordle_list.append(word.replace("\n",""))
 
-print(len(wordle_list))
+test_list = wordle_list[:10]
+print(test_list)
+for letter in alphabet:
+    result[letter] = 0
+    for word in wordle_list:
+        for l in word:
+            if letter == l:
+                result[letter] += 1
 
-
+print(result)
 
 
 
